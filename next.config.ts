c5f155928f2c1a612a659f3ebcaafec705d23d9c
Denes Papp <pppdns@gmail.com>
2025-08-23 16:56:45 +0200

    Set HTTP headers to enable Notion iframe embedding

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import { Header } from 'next/dist/lib/load-custom-routes';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allow embedding in Notion
+  headers: async (): Promise<Header[]> => [
+    {
+      source: '/embed/:path*',
+      headers: [
+        {
+          key: 'Content-Type',
+          value: 'text/html; charset=utf-8',
+        },
+        {
+          key: 'Content-Security-Policy',
+          value:
+            "frame-ancestors 'self' https://notion.so https://*.notion.so https://notion.site https://*.notion.site;",
+        },
+        { key: 'X-Content-Type-Options', value: 'nosniff' },
+        { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
