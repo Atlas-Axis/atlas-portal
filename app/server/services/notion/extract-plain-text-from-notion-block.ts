b966bb3d172179457b2b1ce7eeef2bc9e9644b0f
Denes Papp <pppdns@gmail.com>
2025-08-26 00:50:11 +0200

    Refactor `extractPlainText` function to support additional Notion block types and improve type handling

--- app/server/services/notion/extract-plain-text-from-notion-block.ts
+++ app/server/services/notion/extract-plain-text-from-notion-block.ts
@@ -1,29 +1,52 @@
-import { NotionBlockContent, NotionBlockListItem, RichText } from './notion-types';
+import { BlockObjectResponse } from '@notionhq/client';
+import {
+  BookmarkBlockObjectResponse,
+  ChildDatabaseBlockObjectResponse,
+  ChildPageBlockObjectResponse,
+  EquationBlockObjectResponse,
+  FileBlockObjectResponse,
+  ImageBlockObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints';
 
-export function extractPlainText(block: NotionBlockListItem): string {
+interface RichTextObject {
+  plain_text: string;
+}
+
+export function extractPlainText(block: BlockObjectResponse): string {
   const blockType = block.type;
-  if (!block[blockType]) return '';
 
-  const content = block[blockType] as NotionBlockContent;
+  // If `rich_text` property exists, extract plain text
+  const content = (block as Record<string, unknown>)[blockType] as Record<string, unknown>;
+  const richTextObject = content?.rich_text as RichTextObject[] | null;
 
   // Handle text-based blocks
-  if (content.rich_text && Array.isArray(content.rich_text)) {
-    return content.rich_text.map((richText: RichText) => richText.plain_text || '').join('');
+  if (richTextObject && Array.isArray(richTextObject)) {
+    return richTextObject.map((richText: RichTextObject) => richText.plain_text || '').join(' ');
   }
 
-  // Handle other block types
+  // Handle other block types that may contain text in different properties
   // Docs: https://developers.notion.com/reference/block
   switch (blockType) {
-    case 'child_page':
-      return content.title || '';
-    case 'child_database':
-      return content.title || '';
     case 'bookmark':
-      return content.url || '';
+      return (block as BookmarkBlockObjectResponse).bookmark.url || '';
+    case 'equation':
+      return (block as EquationBlockObjectResponse).equation.expression || '';
     case 'image':
-      return content.caption?.map((text: RichText) => text.plain_text || '').join('') || '';
+      return (
+        (block as ImageBlockObjectResponse).image.caption
+          ?.map((text: RichTextObject) => text.plain_text || '')
+          .join('') || ''
+      );
+    case 'table':
+      return '[TABLE]';
+    case 'table_row':
+      return '[TABLE ROW]';
     case 'file':
-      return content.name || '';
+      return (block as FileBlockObjectResponse).file.name || '';
+    case 'child_page':
+      return (block as ChildPageBlockObjectResponse).child_page.title || '';
+    case 'child_database':
+      return (block as ChildDatabaseBlockObjectResponse).child_database.title || '';
     default:
       console.warn(`Unhandled block type: ${blockType}`);
       return '';
