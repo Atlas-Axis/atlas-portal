5bcb27abe233dc4c65e378114e0bf5d62ac507c3
Denes Papp <pppdns@gmail.com>
2025-08-27 03:07:48 +0200

    Refactor `Layout` component to improve structure and styling

--- app/embed/layout.tsx
+++ app/embed/layout.tsx
@@ -3,5 +3,9 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="flex min-h-screen flex-col items-center justify-center border p-3">{children}</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="w-full max-w-2xl rounded-2xl border bg-slate-100 p-6">{children}</div>
+    </div>
+  );
 }
