a2453c59a8d2d7cde323584a3c545547af74c87b
Denes Papp <pppdns@gmail.com>
2025-08-31 20:11:55 +0200

    Refactor NOTION IDs to use `uuidToHyphens` utility for consistent formatting

--- app/server/services/notion/_demo-data.ts
+++ app/server/services/notion/_demo-data.ts
@@ -1,3 +1,6 @@
-export const NOTION_PAGE_ID = '25ef758464c5808db8bbdb67351fa26f';
-export const NOTION_DATABASE_ID = '25ef758464c580319597e2a98756bbc8';
-export const NOTION_EDIT_PAGES_DATABASE_ID = '25cf758464c580d3a9c0caf1f18f7143';
+import { uuidToHyphens } from '../../utils/utils';
+
+export const NOTION_PAGE_ID = uuidToHyphens('25ef758464c5808db8bbdb67351fa26f');
+export const NOTION_DATABASE_ID = uuidToHyphens('25ef758464c580319597e2a98756bbc8');
+export const NOTION_EDIT_PAGES_DATABASE_ID = uuidToHyphens('25cf758464c580d3a9c0caf1f18f7143');
+export const NOTION_EDIT_PAGE_ORIGINAL_ID = uuidToHyphens('25ef758464c580f6a11aeab7080d03b1');
