9172322339b9f89acc05227e66eeeb230d478c2f
Denes Papp <pppdns@gmail.com>
2025-09-10 18:33:37 +0200

    Add constants and type defs for Atlas databases, add ID mapping

---
+++ app/server/services/atlas/constants.ts
@@ -0,0 +1,78 @@
+export const ATLAS_DATABASES = {
+  SCOPES: 'Scopes',
+  ARTICLES: 'Articles',
+  SECTIONS_AND_PRIMARY_DOCS: 'Sections & Primary Docs',
+  ANNOTATIONS: 'Annotations',
+  TENETS: 'Tenets',
+  SCENARIOS: 'Scenarios',
+  SCENARIO_VARIATIONS: 'Scenario Variations',
+  ACTIVE_DATA: 'Active Data',
+  AGENTS: 'Agent Scope Database',
+} as const;
+
+export const ATLAS_DATABASE_NAMES = [
+  ATLAS_DATABASES.SCOPES,
+  ATLAS_DATABASES.ARTICLES,
+  ATLAS_DATABASES.SECTIONS_AND_PRIMARY_DOCS,
+  ATLAS_DATABASES.ANNOTATIONS,
+  ATLAS_DATABASES.TENETS,
+  ATLAS_DATABASES.SCENARIOS,
+  ATLAS_DATABASES.SCENARIO_VARIATIONS,
+  ATLAS_DATABASES.ACTIVE_DATA,
+  ATLAS_DATABASES.AGENTS,
+] as const;
+
+export type AtlasDatabaseName = (typeof ATLAS_DATABASE_NAMES)[number];
+
+export const ATLAS_DATABASE_ID_MAP: Record<AtlasDatabaseName, string> = {
+  [ATLAS_DATABASES.SCOPES]: 'ebdb403a44bd4d169ec8f9330e955247',
+  [ATLAS_DATABASES.ARTICLES]: '15e06a0d07364458a5caeb85d7b54408',
+  [ATLAS_DATABASES.SECTIONS_AND_PRIMARY_DOCS]: '06d1d4fa1cc44e88a06559d4082163a8',
+  [ATLAS_DATABASES.ANNOTATIONS]: 'e147e8835a2143c38264e86b1d9b24fc',
+  [ATLAS_DATABASES.TENETS]: '7fcbad225c524dffa20cd4efb2e13b56',
+  [ATLAS_DATABASES.SCENARIOS]: '8a05694599194c3ca8c8ee1b86086837',
+  [ATLAS_DATABASES.SCENARIO_VARIATIONS]: 'd0de59236e6d4a48a44533fa64d966ac',
+  [ATLAS_DATABASES.ACTIVE_DATA]: '5b566dd732464927b8eee6e1b2ff99d9',
+  [ATLAS_DATABASES.AGENTS]: '1bbf2ff08d73808d9ce3e2122857e262',
+} as const;
+
+export type AtlasDatabaseID = (typeof ATLAS_DATABASE_ID_MAP)[AtlasDatabaseName];
+
+export const MASTER_STATUSES = {
+  APPROVED: 'Approved',
+  PROVISIONAL: 'Provisional',
+  PLACEHOLDER: 'Placeholder',
+  DEFERRED: 'Deferred',
+  ARCHIVED: 'Archived',
+};
+
+export const MASTER_STATUS_ID_MAP: Record<string, string> = {
+  [MASTER_STATUSES.ARCHIVED]: '434486e6-0d5e-4541-9f00-40cb9bd67d1c',
+  [MASTER_STATUSES.DEFERRED]: 'f38bf53d-96bd-4345-a403-c6629ed202a1',
+  [MASTER_STATUSES.APPROVED]: 'fe75a64f-585b-4d08-af00-ef8667d9c307',
+  [MASTER_STATUSES.PROVISIONAL]: '3dbb9d9c-fd63-462b-99f3-1ce879f16768',
+  [MASTER_STATUSES.PLACEHOLDER]: '3edf54e3-be0e-4bbb-b008-502cfc23394e',
+};
+
+export type MasterStatus = (typeof MASTER_STATUSES)[keyof typeof MASTER_STATUSES];
+
+export const MASTER_STATUS_IDS = Object.values(MASTER_STATUS_ID_MAP);
+
+export const MASTER_STATUS_ID_TO_NAME_MAP: Record<string, string> = {
+  '434486e6-0d5e-4541-9f00-40cb9bd67d1c': MASTER_STATUSES.ARCHIVED,
+  'f38bf53d-96bd-4345-a403-c6629ed202a1': MASTER_STATUSES.DEFERRED,
+  'fe75a64f-585b-4d08-af00-ef8667d9c307': MASTER_STATUSES.APPROVED,
+  '3dbb9d9c-fd63-462b-99f3-1ce879f16768': MASTER_STATUSES.PROVISIONAL,
+  '3edf54e3-be0e-4bbb-b008-502cfc23394e': MASTER_STATUSES.PLACEHOLDER,
+};
+
+export const ALLOWED_PAGE_FIELD_TYPES = [
+  'title',
+  'rich_text',
+  'select',
+  'relation',
+  'number',
+  'url',
+  'files',
+  'formula',
+] as const;
