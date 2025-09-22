886d2449bde935fbb99f2d2faf8892e360440ea1
Denes Papp <pppdns@gmail.com>
2025-09-22 19:30:36 +0200

    Set dynamic export to 'force-static' for AtlasListPage to enable static rendering instead of ISR. Getting build errors because the ISR cached response is bigger than 19 MB

--- app/atlas/list/page.tsx
+++ app/atlas/list/page.tsx
@@ -1,6 +1,8 @@
 import { loadAtlasFromSupabase } from '@/app/server/services/atlas/load-atlas-from-supabase';
 import AtlasListClient from './atlas-list';
 
+export const dynamic = 'force-static';
+
 console.log('/atlas/list is being prerendered');
 
 export default async function AtlasListPage() {
