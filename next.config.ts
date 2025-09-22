06a89a5d57ec0b57b9207e95427b3e0f41bb0e24
Denes Papp <pppdns@gmail.com>
2025-09-22 22:00:37 +0200

    Add configuration options to next.config.ts for minification. Next.js 15.5.3 update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // Configuration options can be added here as needed
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
