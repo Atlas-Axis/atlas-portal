b78e4b0072680621ea47fad461128564f6a08bbf
Denes Papp <pppdns@gmail.com>
2025-09-24 21:00:58 +0200

    Add constants for Atlas JSON output directory and files

---
+++ scripts/atlas-json/constants.ts
@@ -0,0 +1,4 @@
+export const ATLAS_JSON_OUTPUT_DIR = '.debug-data/atlas-json-generated';
+export const ATLAS_JSON_OUTPUT_FILE_GITHUB = 'atlas-github.json';
+export const ATLAS_JSON_OUTPUT_FILE_SUPABASE = 'atlas-supabase.json';
+export const ATLAS_JSON_OUTPUT_FILE_BLUE = 'atlas-blue.json';
