e243c5c11c8f1b5cc31d58ed568d520c7bee50fe
Denes Papp <pppdns@gmail.com>
2025-10-20 18:17:40 +0200

    chore: remove test formatting file

--- test-formatting.ts
+++
@@ -1,9 +0,0 @@
-// Test file with poor formatting
-const test = 'hello';
-const obj = { name: 'test', value: 123 };
-
-function badFormatting(a: number, b: number) {
-  return a + b;
-}
-
-export { test, obj, badFormatting };
