53e481b7c46646a0acce1b79d9eeb7fcbb90229a
Denes Papp <pppdns@gmail.com>
2025-10-20 18:17:24 +0200

    test: verify auto-formatting in pre-commit hook

---
+++ test-formatting.ts
@@ -0,0 +1,9 @@
+// Test file with poor formatting
+const test = 'hello';
+const obj = { name: 'test', value: 123 };
+
+function badFormatting(a: number, b: number) {
+  return a + b;
+}
+
+export { test, obj, badFormatting };
