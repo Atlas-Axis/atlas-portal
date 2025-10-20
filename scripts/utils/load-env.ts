b6cd7c38b4e353fd83c81c3ea5bf0a4926774921
Denes Papp <pppdns@gmail.com>
2025-10-20 18:15:30 +0200

    Improve environment variable logging by adding conditional debug logging in `load-env.ts`. The script now logs the `NODE_ENV` only when debug logging is enabled, enhancing performance and reducing unnecessary console output in production environments.

--- scripts/utils/load-env.ts
+++ scripts/utils/load-env.ts
@@ -1,8 +1,11 @@
 import { loadEnvConfig } from '@next/env';
+import { DEBUG_LOGGING } from '@/app/shared/utils/is-debug-logging-enabled';
 
 export const isDevelopment = process.env.NODE_ENV !== 'production';
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`isDevelopment: ${isDevelopment}`);
+
+if (DEBUG_LOGGING()) {
+  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+}
 
 export function loadEnv() {
   // Load environment variables
