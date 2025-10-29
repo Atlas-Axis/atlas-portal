5637a980405ed1a1035626999f98c4b022b93eb9
Denes Papp <pppdns@gmail.com>
2025-10-29 23:20:06 +0100

    AtlasPagePrerendered component: Remove unused state and imports

--- app/atlas/atlas-page-prerendered.tsx
+++ app/atlas/atlas-page-prerendered.tsx
@@ -1,37 +1,21 @@
 'use client';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { useCallback, useMemo, useState } from 'react';
+import { useState } from 'react';
 import { StandardizedAtlasDocument } from '@/app/server/atlas/json-export/types';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { type UuidMappings, serializeUuidMappings } from '@/app/server/atlas/load-uuid-mapping';
-// import AgentsHydrator from './agents-hydrator';
+import { type UuidMappings } from '@/app/server/atlas/load-uuid-mapping';
 import ContentTree from './content-tree';
 import Sidebar from './sidebar';
 
 interface AtlasPagePrerenderedProps {
   standardizedScopeTreesWithoutAgents: StandardizedAtlasDocument[];
-  // standardizedAgentDocs: StandardizedAtlasDocument[];
   uuidMappings: UuidMappings;
 }
 
 export default function AtlasPagePrerendered({
   standardizedScopeTreesWithoutAgents,
-  // standardizedAgentDocs,
   uuidMappings,
 }: AtlasPagePrerenderedProps) {
   const [scopeTreesWithoutAgents] = useState(standardizedScopeTreesWithoutAgents);
-  // const [agentDocs, setAgentDocs] = useState<StandardizedAtlasDocument[]>(standardizedAgentDocs);
-  // const [agentsLoaded, setAgentsLoaded] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [agentsLoaded, setAgentsLoaded] = useState(true);
-
-  // const serializedUuidMappings = useMemo(() => serializeUuidMappings(uuidMappings), [uuidMappings]);
-
-  // const handleAgentsLoaded = useCallback((loadedAgentDocs: StandardizedAtlasDocument[]) => {
-  //   setAgentDocs(loadedAgentDocs);
-  //   setAgentsLoaded(true);
-  // }, []);
 
   return (
     <div className="min-h-screen overflow-x-hidden bg-white">
@@ -40,23 +24,9 @@ export default function AtlasPagePrerendered({
         <ContentTree
           scopeTreesWithoutAgents={scopeTreesWithoutAgents}
           uuidMappings={uuidMappings}
-          agentsLoading={!agentsLoaded}
-          // agentDocs={agentDocs}
-          // agentDocs={[]}
+          agentsLoading={false}
         />
       </div>
-
-      {/* Embed agent data as JSON in the HTML for client-side hydration */}
-      {/* <script id="agent-data" type="application/json" dangerouslySetInnerHTML={{ __html: JSON.stringify([]) }} /> */}
-
-      {/* Embed UUID mappings for client-side rendering */}
-      {/* <script
-        id="uuid-mappings-data"
-        type="application/json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(serializedUuidMappings) }}
-      /> */}
-
-      {/* <AgentsHydrator onAgentsLoaded={handleAgentsLoaded} /> */}
     </div>
   );
 }
